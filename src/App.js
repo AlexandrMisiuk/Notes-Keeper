97fc8cbe923bbc66b37a3fdc9792a217d304005d
AlexandrMisiuk <alexandr.misiuk@gmail.com>
2021-10-04 19:10:29 +0300

    add react routet and fix colot btn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { Box, Container, Grid } from "@material-ui/core";
 import { Provider } from "react-redux";
@@ -16,21 +17,23 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <div className={classes.root}>
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <Header />
+      <Router>
+        <div className={classes.root}>
+          <Grid container spacing={0}>
+            <Grid item xs={12}>
+              <Header />
+            </Grid>
+            <Grid item xs={12}>
+              <Container maxWidth="lg">
+                <Box display="flex">
+                  <Main />
+                  <Navbar />
+                </Box>
+              </Container>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Container maxWidth="lg">
-              <Box display="flex">
-                <Main />
-                <Navbar />
-              </Box>
-            </Container>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </Router>
     </Provider>
   );
 }
